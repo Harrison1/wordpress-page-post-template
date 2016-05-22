d7e3bcce0fcc1035191224a3fb891b6b37e76cc0
Harrison <hmcguire13@gmail.com>
2016-05-22 09:26:16 -0700

    template commit

---
+++ posttemplate.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * Template Name: harrytemplate
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
+<h2>Recent Posts</h2>
+<ul>
+<?php
+    echo "HERE'S HARRY";
+$args = array( 'numberposts' => '1' );
+	$recent_posts = wp_get_recent_posts( $args );
+	foreach( $recent_posts as $recent ){
+		echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+	}
+?>
+</ul>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
